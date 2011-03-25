c4cdc062dc7a9ed30c2166b256711d74b99bd3f7
Mark Essel <messel@gmail.com>
2011-03-25 16:13:30 -0400

    added ?q= parameter for search term

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ var last_key;
 var time_out_id = 0;
 var delta = 5000;
 
+var urlParams = {};
+(function () {
+    var e,
+        a = /\+/g,  // Regex for replacing addition symbol with a space
+        r = /([^&=]+)=?([^&]*)/g,
+        d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+        q = window.location.search.substring(1);
+
+    while (e = r.exec(q))
+       urlParams[d(e[1])] = d(e[2]);
+})();
+
 
 function one(user,key) {
 	last_key = key;
